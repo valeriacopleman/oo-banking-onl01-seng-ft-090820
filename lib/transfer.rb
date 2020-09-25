d612373b57752273f747ca9ef6185580b4d32e3b
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 00:50:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end 
 
   def execute_transaction
-    if valid? && @sender.balance >=amount 
+    if valid? && @sender.balance >= @amount 
       @sender.balance -= @amount
       @receiver.balance += amount
       @status = "complete"
