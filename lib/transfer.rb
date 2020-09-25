0bcdc4d8bd1e5eb4c54984d98a7825ae56cb449f
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 00:18:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,15 +15,7 @@ class Transfer
   end 
 
  def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
-  end
+
   
   def reverse_transfer
     if @status == "complete"
