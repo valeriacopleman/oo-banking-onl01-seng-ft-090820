d32625203e97914f245eedb6677beb5f2f3a472c
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 00:22:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     if @sender.valid == true { |transfer|
-    @sender.balance -= @amount 
+    @sender.balance -= @amount &&
     @receiver.balance += @amount }
   else
     puts "Transaction rejected. Please check your account balance."
