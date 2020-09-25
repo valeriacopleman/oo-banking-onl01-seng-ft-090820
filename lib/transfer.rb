ce731f828baf56feac689b1ca7180c3469cc9fe1
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 01:03:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += amount
       @status = "complete"
-    elsif @sender.balance < @amount
+    elsif @sender.balance < @amount || @account.valid?
       @status = "rejected"
       puts "Transaction rejected. Please check your account balance."
     else
